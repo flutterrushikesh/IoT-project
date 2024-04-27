55370e0e4031206d3df6a1524020cbf23ad0b866
flutterrushikesh <flutterrushikesh@gmail.com>
2024-04-28 01:50:51 +0530

    working

---
+++ mera_rashan/lib/views/signup_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SignUp extends StatefulWidget {
+  const SignUp({super.key});
+  @override
+  State createState() => _SignUpState();
+}
+
+class _SignUpState extends State {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
